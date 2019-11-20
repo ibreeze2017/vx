d5934924263043437da289f34e291ecf91b89a93
ixw2017@163.com <Wsrf@2017@git$>
2019-11-20 23:23:00 +0800

    chore:add test

---
+++ _utils/__tests__/net.ts
@@ -0,0 +1,10 @@
+import { getIPAddress, hexIp } from '../format/ip';
+
+
+test('测试IP转换为十六进制', function() {
+  expect(hexIp('192.168.31.217')).toBe('0xd91fa8c0');
+});
+
+test('测试IP转换为字符串', function() {
+  expect(getIPAddress('0xd91fa8c0')).toBe('192.168.31.217');
+});
